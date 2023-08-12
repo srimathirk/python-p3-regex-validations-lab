a285c51fcf3320bbf33b185c1382b28c90bd5d95
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-08-12 15:47:41 -0400

    regular expression to find match lab

--- lib/regex.py
+++ lib/regex.py
@@ -2,12 +2,15 @@ import re
 
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# text = "John Cena Anya Taylor-Joy D'Angelo"
+name = r"^(?!.*\s{3,}).*[A-Z][a-zA-Z-' ]*$"
 name_regex = re.compile(name)
+# match = name_regex.fullmatch(text)
+# print(match)
 
-phone_number = r""
+
+phone_number = r"\(\d{3}\) \d{3}-\d{4}|\d{10}|\d{3}-\d{3}-\d{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"\b(?![0-9])[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z0-9]+\b"
 email_regex = re.compile(email_address)
